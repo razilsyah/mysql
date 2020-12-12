ce2f933438e2b9d91a9080a27c6e582d35502a41
razilsyah <razilsyah5@gmail.com>
2020-12-12 23:46:27 +0700

    modified inner-join

--- inner_join.sql
+++ inner_join.sql
@@ -2,3 +2,19 @@
 
 -- contoh 
 -- ingin mengetahui nama kota dari seorang employee (nama kota di table office dan seorang employee di table employee)
+
+-- menampilakan database
+SHOW DATABASES;
+
+-- ambil database classicModels
+USE classicmodels;
+
+-- tampilkan table 
+SHOW TABLES;
+
+DESCRIBE `employees`
+DESCRIBE offices
+
+-- inner join
+SELECT firstName,city FROM employees INNER JOIN offices
+ON employees.`officeCode` = offices.`officeCode`;
