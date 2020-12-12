d9d546f985bb508268f24922f433821b77a27362
razilsyah <razilsyah5@gmail.com>
2020-12-12 23:33:27 +0700

    inner_join

---
+++ inner_join.sql
@@ -0,0 +1,4 @@
+-- join digunakan ketika membutuhkan informasi yang berasal dari 2 tabel atau lebih
+
+-- contoh 
+-- ingin mengetahui nama kota dari seorang employee (nama kota di table office dan seorang employee di table employee)
